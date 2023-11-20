a3c26107a2bc1c7815acab6ae04aa5e5e23c11d1
Rudi Farkas <rudi.farkas@gmail.com>
2023-11-20 19:39:26 +0100

    add AppDelegate.isAppAuthorised

--- Juice/AppDelegate.swift
+++ Juice/AppDelegate.swift
@@ -38,18 +38,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     /// - Tag: did_finish_launching
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        dispatchOnAuthorization { result in
-            switch result {
-            case let .success(isAuthorized):
-                self.printClassAndFunc("Authorization status: \(isAuthorized)")
-            case let .failure(error):
-                self.printClassAndFunc("Authorization error: \(error)")
-                // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
-                DispatchQueue.main.async {
-                    self.window?.rootViewController?.showLoginViewController()
+        Task {
+            do {
+                let isAuthorised = try await isAppAuthorised()
+                if isAuthorised {
+                    self.printClassAndFunc("App is authorised")
+                } else {
+                    self.printClassAndFunc("App is not authorised")
+                    DispatchQueue.main.async {
+                        self.window?.rootViewController?.showLoginViewController()
+                    }
                 }
+            } catch {
+                print("Failed to check authorisation: \(error)")
+                // Handle error
             }
         }
         return true
+    }
+
+    func isAppAuthorised() async throws -> Bool {
+        return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<Bool, Error>) in
+            let appleIDProvider = ASAuthorizationAppleIDProvider()
+            appleIDProvider.getCredentialState(forUserID: KeychainItem.currentUserIdentifier) { credentialState, _ in
+                switch credentialState {
+                case .authorized:
+                    continuation.resume(returning: true) // The Apple ID credential is valid.
+                case .revoked, .notFound:
+                    // The Apple ID credential is either revoked or was not found.
+                    continuation.resume(returning: false)
+                default:
+                    break
+                }
+            }
+        })
     }
 }
